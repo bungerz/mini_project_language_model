ea98a3e63905b18ba50d461fba9c23b524a0fea4
bungerz <adel.bensahla@gmail.com>
2025-12-02 21:38:33 +0100

    feat: training done, v1 ok

--- src/my_tokenizer.py
+++ src/my_tokenizer.py
@@ -7,12 +7,22 @@ class CharDataset(Dataset):
     Emits batches of characters.
 
     Adapted from "https://github.com/karpathy/minGPT".
-    """    
-    def __init__(self, data: str, block_size):
+    """
 
-        chars = sorted(list(set([a_char for a_char in data]))) # get characters from the input data
-        self.stoi = { ch:i for i,ch in enumerate(chars) } # map characters to integer indices
-        self.itos = { i:ch for i,ch in enumerate(chars) } # map integer indices to characters
+    def __init__(self, data: str, block_size, vocab=None):
+
+        if vocab is None:
+            chars = sorted(
+                list(set([a_char for a_char in data]))
+            )  # get characters from the input data
+        else:
+            chars = vocab  # use provided vocabulary
+        self.stoi = {
+            ch: i for i, ch in enumerate(chars)
+        }  # map characters to integer indices
+        self.itos = {
+            i: ch for i, ch in enumerate(chars)
+        }  # map integer indices to characters
         self.block_size = block_size
         self.data = data
 
@@ -24,7 +34,7 @@ class CharDataset(Dataset):
 
     def __getitem__(self, idx):
         # grab a chunk of (block_size + 1) characters from the data
-        chunk = self.data[idx:idx + self.block_size + 1]
+        chunk = self.data[idx : idx + self.block_size + 1]
         # encode every character to an integer
         encoded_chunk = [self.stoi[c] for c in chunk]
         # return the chunk and the shifted version as tensors
@@ -34,4 +44,4 @@ class CharDataset(Dataset):
         return [self.stoi[s] for s in string]
 
     def decode(self, indices):
-        return ''.join([self.itos[i] for i in indices])
+        return "".join([self.itos[i] for i in indices])
